0f73cb41aca2d0f825771d0982c01eb92d953b25
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2022-11-12 22:24:20 -0300

    Componente de leitura

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,7 +1,55 @@
-function UserList(params) {
-    
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+function UserList() {
+    const [APIData, setAPIData] = useState()
+    useEffect(() => {
+        axios.get(`http://localhost:3001/users`)
+            .then((response) => {
+                setAPIData(response.data)
+            })
+    }, [])
+
+    const setData = (data) => {
+        let { id, name, email } = data;
+        localStorage.setItem('ID', id);
+        localStorage.setItem('Name', name);
+        localStorage.setItem('E-mail', email);
+    }
+
+    const handleDelete = async (id) => {
+        await axios.delete(`http://localhost:3001/users/${id}`)
+            .then(() => alert('Usuário removido com sucesso.'))
+            .then(() => {
+                getData();
+            })
+    }
+
+    const getData = () => {
+        axios.get(`http://localhost:3001/users`)
+            .then((getData) => {
+                setAPIData(getData.data);
+            })
+    }
+
+
     return(
-        <p>Lista de usuários</p>
+        <>
+            {APIData && APIData.map((user) => {
+                return (
+                    <div key={user.id}>
+                        <p>{user.id}</p>
+                        <p>{user.name}</p>
+                        <p>{user.email}</p>
+                        <Link to="/edituser">
+                            <button onClick={() => setData(user)}>Editar</button>
+                        </Link>
+                        <button onClick={() => handleDelete(user.id)}>Excluir</button>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
