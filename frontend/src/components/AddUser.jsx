00801358bc0f4bf76853ca0eb8a54ebfd39d2141
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2023-04-11 23:42:16 -0300

    Validação dos campos em AddUser

--- frontend/src/components/AddUser.jsx
+++ frontend/src/components/AddUser.jsx
@@ -1,25 +1,48 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { object, string, InferType } from 'yup';
+
+let userSchema = object({
+    name: string().required('O campo nome não pode ficar vazio.')
+        .matches(/^[a-zA-Z\u00C0-\u017F\s]+$/, 'O nome deve conter apenas letras'),
+    email: string().email('Digite um e-mail válido').required('O campo email não pode ficar vazio.')
+})
 
 function AddUser() {
     let navigate = useNavigate();
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const [status, setStatus] = useState({
+        type: '',
+        message: ''
+    })
 
     const postData = async (e) => {
         e.preventDefault()
-        let response = await axios.post(`http://localhost:3001/users`, {
-            name,
-            email
-        })
 
-        if (response) {
-            alert("Usuário cadastrado com sucesso.")
-            navigate('/home')
-        } else {
-            alert("Não foi possível cadastrar o usuário.")
+        try {
+            await userSchema.validate({ name, email });
+
+            let response = await axios.post(`http://localhost:3001/users`, {
+                name,
+                email
+            })
+
+            if (response) {
+                alert("Usuário cadastrado com sucesso.")
+                navigate('/home')
+            } else {
+                alert("Não foi possível cadastrar o usuário.")
+            }
+
+        } catch (err) {
+            setStatus({
+                type: 'error',
+                message: err.errors
+            })
         }
+
     }
 
     const handleCancel = () => {
@@ -32,6 +55,8 @@ function AddUser() {
         <div className="container">
             <div className="new-user">
                 <h1>Novo usuário</h1>
+                {status.type === 'error' ? <p style={{ color: 'yellow' }}>{status.message}</p> : ''}
+                <br />
                 <form className="user-form">
                     <label>Nome</label>
                     <input
